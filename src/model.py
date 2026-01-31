66cd86d059d0c0b4f27431dc25741ea918c8b4fd
lewisksm2 <157165021+lewisksm2@users.noreply.github.com>
2026-01-30 23:07:32 -0500

    Add initial model.py skeleton.

---
+++ src/model.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import (
+    accuracy_score,
+    roc_auc_score,
+    confusion_matrix
+)
+
+
+#--------------------------------------------------
+# Model Constructors
+#--------------------------------------------------
+
+def get_logistic_model(random_state=42):
+    
+    pass
+
+def get_random_forest_model(random_state=42):
+    
+    pass
+
+def train_model(model, X, y):
+    
+    pass
+
+def evaluate_model(model, X, y):
+    
+    pass
