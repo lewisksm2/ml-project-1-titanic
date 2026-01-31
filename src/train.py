e6b16266c094829bbeb935eab2cca06f9c922415
lewisksm2 <157165021+lewisksm2@users.noreply.github.com>
2026-01-30 23:31:49 -0500

    Split dataset into features and target

--- src/train.py
+++ src/train.py
@@ -1,18 +1,27 @@
 import pandas as pd
 
 data_path = "data/processed/features.csv"
+target_col = "Survived"
 
 def load_data(path=data_path):
     df = pd.read_csv(path)
     return df
+
+def split_features_target(df):
+    X = df.drop(columns=[target_col])
+    y = df[target_col]
+    
+    return X, y
 
 def main():
     print("Loading data...")
     
     df = load_data()
     
-    print("Data shape:", df.shape)
-    print(df.head())
+    X, y = split_features_target(df)
+    
+    print("X Shape:", X.shape)
+    print("y Shape:", y.shape)
 
 
 if __name__ == "__main__":
